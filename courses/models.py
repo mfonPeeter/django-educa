2603a7a931afe20d8ce867a6295d8ab5127d3f54
mfonPeeter <godimfon@gmail.com>
2025-07-12 11:04:19 +0100

    feat: implement ordering for Module and Content objects

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -48,9 +49,13 @@ class Module(models.Model):
     )
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=["course"])
+
+    class Meta:
+        ordering = ["order"]
 
     def __str__(self):
-        return self.title
+        return f"{self.order}. {self.title}"
 
 
 class Content(models.Model):
@@ -66,6 +71,10 @@ class Content(models.Model):
     )
     object_id = models.PositiveBigIntegerField()
     item = GenericForeignKey("content_type", "object_id")
+    order = OrderField(blank=True, for_fields=["module"])
+
+    class Meta:
+        ordering = ["order"]
 
 
 class ItemBase(models.Model):
