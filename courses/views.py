e359eb46e0e80c37158eea2ff7b46df61b08d226
mfonPeeter <godimfon@gmail.com>
2025-07-19 12:45:04 +0100

    feat: display course catalog to users

--- courses/views.py
+++ courses/views.py
@@ -3,13 +3,15 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import get_object_or_404, redirect
 from django.apps import apps
 from django.forms.models import modelform_factory
+from django.db.models import Count
 
 from .forms import ModuleFormSet
-from .models import Course, Module, Content
+from .models import Subject, Course, Module, Content
 
 
 class OwnerMixin:
@@ -151,3 +153,29 @@ class ContentOrderView(CsrfExemptMixin, JSONRequestResponseMixin, View):
                 order=order
             )
         return self.render_json_response({"saved": "OK"})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = "courses/course/list.html"
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count("courses"))
+        courses = Course.objects.annotate(total_modules=Count("modules"))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+
+        return self.render_to_response(
+            {
+                "subjects": subjects,
+                "subject": subject,
+                "courses": courses,
+            }
+        )
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "courses/course/detail.html"
