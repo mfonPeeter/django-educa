07acf931cf8bb64ec5ea570b34dbf851fac581d9
mfonPeeter <godimfon@gmail.com>
2025-08-17 15:03:38 +0100

    feat: build course serializer with relational and nested serialization support

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,6 @@
 from django.db.models import Count
 from rest_framework import serializers
-from courses.models import Subject
+from courses.models import Course, Module, Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -16,3 +16,26 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ["id", "title", "slug", "total_courses", "popular_courses"]
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ["order", "title", "description"]
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            "id",
+            "subject",
+            "title",
+            "slug",
+            "overview",
+            "created",
+            "owner",
+            "modules",
+        ]
