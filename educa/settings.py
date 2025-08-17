9953de002141977bdc403b3a07eaff3454144733
mfonPeeter <godimfon@gmail.com>
2025-08-17 13:21:11 +0100

    feat: set up DRF

--- educa/settings.py
+++ educa/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "embed_video",
     "debug_toolbar",
     "redisboard",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -157,3 +158,9 @@ CACHE_MIDDLEWARE_KEY_PREFIX = "educa"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permission.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
