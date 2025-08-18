40206e30987406c1a8cdafb184ca77eb8354efd3
mfonPeeter <godimfon@gmail.com>
2025-08-18 09:42:47 +0100

    feat: extend ViewSets with custom actions, permissions, and course content serialization

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,14 +1,22 @@
 from django.db.models import Count
-from django.shortcuts import get_object_or_404
+
+# from django.shortcuts import get_object_or_404
 
 # from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.views import APIView
+
+# from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 
-from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.api.serializers import (
+    CourseSerializer,
+    SubjectSerializer,
+    CourseWithContentsSerializer,
+)
+from courses.api.permissions import IsEnrolled
 from courses.models import Course, Subject
 from courses.api.pagination import StandardPagination
 
@@ -35,12 +43,33 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
 
-
-class CourseEnrollView(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+    @action(
+        detail=True,
+        methods=["post"],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated],
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({"enrolled": True})
+
+    @action(
+        detail=True,
+        methods=["get"],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled],
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+# class CourseEnrollView(APIView):
+#     authentication_classes = [BasicAuthentication]
+#     permission_classes = [IsAuthenticated]
+
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({"enrolled": True})
