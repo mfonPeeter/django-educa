93fd0b305adc7e177484185ad6ef558cab9374fb
mfonPeeter <godimfon@gmail.com>
2025-08-18 08:38:46 +0100

    feat: build custom API views with authentication and permission handling

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,13 @@
 from django.db.models import Count
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+
+# from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from courses.api.serializers import CourseSerializer, SubjectSerializer
 from courses.models import Course, Subject
 from courses.api.pagination import StandardPagination
@@ -27,3 +34,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related("modules")
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({"enrolled": True})
