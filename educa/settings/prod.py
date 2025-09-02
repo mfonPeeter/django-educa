6539f1b24cd6329f15a082f398ab9b6bc9e11e35
mfonPeeter <godimfon@gmail.com>
2025-09-02 22:28:01 +0100

    feat: configure hostname and serve static/media assets with NGINX

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -7,7 +7,7 @@ ADMINS = [
     ("Mfonobong Peter", "godimfonpeter@gmail.com"),
 ]
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["educaproject.com", "www.educaproject.com"]
 
 DATABASES = {
     "default": {
